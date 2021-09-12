c8eab42cf9daeff1a6d70cfcbd0b71fbdc0874d4
Cole Lawrence <cole@colelawrence.com>
2021-09-11 23:51:06 -0400

    break everything & remove dependencies on lazy_static & regex

--- typescript-definitions-derive/src/patch.rs
+++ typescript-definitions-derive/src/patch.rs
@@ -14,9 +14,8 @@
 //! to make the final result a little more acceptable.
 //!
 
-use lazy_static::lazy_static;
 use proc_macro2::Literal;
-use regex::{Captures, Regex};
+// use regex::{Captures, Regex};
 use std::borrow::Cow;
 
 // In typescript '===' is a single token whereas
@@ -36,6 +35,7 @@ const TRIPPLE_EQ: &str = "\"__============__\"";
 const NL_PATCH: &str = "\"__nlnlnlnl__\"";
 const PURE_PATCH: &str = "\"__pure__\"";
 const TS_IGNORE_PATCH: &str = "\"__ts_ignore__\"";
+/*
 // type N = [(&'static str, &'static str); 10];
 const NAMES: [(&str, &str); 16] = [
     ("brack", r"\s*\[\s+\]"),
@@ -52,104 +52,106 @@ const NAMES: [(&str, &str); 16] = [
     ("nlpatch", NL_PATCH),         // for adding newlines to output string
     ("tsignore", TS_IGNORE_PATCH), // for adding ts-ignore comments to output string
     ("pure", PURE_PATCH),          // for adding ts-ignore comments to output str"doc", ing
-    ("doc", r#"#\s*\[\s*doc\s*=\s*"(?P<comment>.*?)"\]"#), // for fixing mishandled ts doc comments
+    // ("doc", r#"#\s*\[\s*doc\s*=\s*"(?P<comment>.*?)"\]"#), // for fixing mishandled ts doc comments
     ("nl", r"\n+"),                // last!
 ];
-lazy_static! {
-    static ref RE: Regex = {
-        let v = NAMES
-            .iter()
-            .map(|(n, re)| format!("(?P<{}>{})", n, re))
-            .collect::<Vec<_>>()
-            .join("|");
-        Regex::new(&v).unwrap()
-    };
-}
-
-trait Has {
-    fn has(&self, s: &'static str) -> bool;
-    fn key(&self) -> &'static str;
-}
-
-impl Has for Captures<'_> {
-    #[inline]
-    fn has(&self, s: &'static str) -> bool {
-        self.name(s).is_some()
-    }
-
-    fn key(&self) -> &'static str {
-        for n in &NAMES {
-            if self.has(n.0) {
-                return n.0;
-            }
-        }
-        "?"
-    }
-    /*
-    fn key(&self) -> &'static str {
-        for n in RE.capture_names() {
-            if let Some(m) = n {
-                if self.has(m) {
-                    return m;
-                }
-            }
-        };
-
-        "?"
-    }
-    */
-}
+*/
+static PATCHES: [(&str, &str); 4] = [
+    (TRIPPLE_EQ, "==="),
+    (NL_PATCH, "\n"),                    // for adding newlines to output string
+    (TS_IGNORE_PATCH, "//@ts-ignore\n"), // for adding ts-ignore comments to output string
+    (PURE_PATCH, "/*#__PURE__*/"),       // for adding ts-ignore comments to output str"doc", ing
+                                         // ("doc", r#"#\s*\[\s*doc\s*=\s*"(?P<comment>.*?)"\]"#), // for fixing mishandled ts doc comments
+                                         // ("nl", r"\n+"),                // last!
+];
 
 // TODO: where does the newline come from? why the double spaces?
 // maybe use RegexSet::new(&[.....])
 pub fn patch(s: &str) -> Cow<'_, str> {
-    RE.replace_all(s, |c: &Captures| {
-        let key = c.key();
-        let m = match key {
-            "brace" => "{}",
-            "brack" => "[]",
-            "colon" => ": ",
-            "fnl" => "{ ",
-            // "bar" => "\n  | {",
-            "enl" => " }",
-            "nl" => " ",
-            // "result" => "|",
-            "te" => "===",
-            "lt" => "<",
-            "gt" => ">",
-            "semi" => ";",
-            "dot" => ".",
-            "call" => " () ",
-            "nlpatch" => "\n",
-            "tsignore" => "//@ts-ignore\n",
-            "pure" => "/*#__PURE__*/",
-            "doc" => {
-                return c.name("comment").map_or(Cow::Borrowed(""), |m| {
-                    (String::from("\n    /**") + &unescape(&m.as_str()) + "*/\n").into()
-                })
-            }
-            _ => return Cow::Owned(c.get(0).unwrap().as_str().to_owned()), // maybe should just panic?
-        };
-        Cow::Borrowed(m)
-    })
+    let mut working = s.to_string();
+
+    for (from, to) in PATCHES {
+        working = working.replace(from, to);
+    }
+
+    Cow::Owned(working)
+    // RE.replace_all(s, |c: &Captures| {
+    //     let key = c.key();
+    //     let m = match key {
+    //         "brace" => "{}",
+    //         "brack" => "[]",
+    //         "colon" => ": ",
+    //         "fnl" => "{ ",
+    //         // "bar" => "\n  | {",
+    //         "enl" => " }",
+    //         "nl" => " ",
+    //         // "result" => "|",
+    //         "te" => "===",
+    //         "lt" => "<",
+    //         "gt" => ">",
+    //         "semi" => ";",
+    //         "dot" => ".",
+    //         "call" => " () ",
+    //         "nlpatch" => "\n",
+    //         "tsignore" => "//@ts-ignore\n",
+    //         "pure" => "/*#__PURE__*/",
+    //         "doc" => {
+    //             return c.name("comment").map_or(Cow::Borrowed(""), |m| {
+    //                 (String::from("\n    /**") + &unescape(&m.as_str()) + "*/\n").into()
+    //             })
+    //         }
+    //         _ => return Cow::Owned(c.get(0).unwrap().as_str().to_owned()), // maybe should just panic?
+    //     };
+    //     Cow::Borrowed(m)
+    // })
 }
 
-lazy_static! {
-    static ref UNESCAPE: Regex = Regex::new(r"\\(.)").unwrap();
+// lazy_static! {
+//     static ref UNESCAPE: Regex = Regex::new(r"\\(.)").unwrap();
+// }
+/*
+struct Unescape<'a> {
+    last_char_was_escape: bool,
+    chars_left: Chars<'a>,
+}
+
+fn unescape_iter<'a>(chars: Chars<'a>) -> Unescape<'a> {
+    Unescape {
+        last_char_was_escape: false,
+        chars_left: chars,
+    }
+}
+
+impl <'a> Iterator for Unescape<'a> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.last_char_was_escape {
+            self.last_char_was_escape = false;
+            Some(match self.chars_left.next() {
+                Some('n') => '\n',
+                Some('t') => '\t',
+                Some(other) => other,
+                None => '\\',
+            })
+        } else {
+            match self.chars_left.next() {
+                Some('\\') => {
+                    self.last_char_was_escape = true;
+                    self.next()
+                }
+                other => other,
+            }
+        }
+    }
 }
 
 // when we get the string, e.g. newlines, backslashes and quotes are escaped
+// used for doc comments!
 fn unescape(input: &str) -> Cow<'_, str> {
-    UNESCAPE.replace_all(input, |c: &Captures| {
-        Cow::Borrowed(match c.get(1).map_or("", |m| m.as_str()) {
-            "n" => "\n",
-            "\"" => "\"",
-            "\\" => "\\",
-            x => return Cow::Owned(x.into()),
-        })
-    })
+    Cow::Owned(unescape_iter(input.chars()).collect())
 }
-
+*/
 #[inline]
 pub fn nl() -> Literal {
     Literal::string(&NL_PATCH[1..NL_PATCH.len() - 1])
